85ac2c3cf0b54de7c9ccc371850e4e00f04d7eab
Aabhas Arora <aabhas.arora@aspiringminds.in>
2015-09-26 15:39:36 +0530

    Second push

--- script/googleMap.js
+++ script/googleMap.js
@@ -1,5 +1,5 @@
 google.maps.event.addDomListener(window, 'load', initMap);
-var map;
+var map, circle, searchedAds = [];
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
@@ -19,11 +19,24 @@ function markUserLocation(lat, long, markerText){
     marker.addListener("position_changed", function () {
         var position = this.getPosition();
         console.log(position);
+        markAdLocation(position.lat(), position.lng());
     });
     markAdLocation(lat, long);
 };
 
 function markAdLocation(lat, long) {
+    searchedAds.forEach(function (value) {
+       value.setMap(null);
+    });
+    var latlongForCenter;
+    if (circle) circle.setMap(null);
+    latlongForCenter = new google.maps.LatLng(lat, long);
+    circle = new google.maps.Circle({center:latlongForCenter,
+        radius: 5000,
+        fillOpacity: 0.35,
+        fillColor: "#FF0000",
+        map: map});
+
     var oSearch = {
         searchParam: {
             latLong: {
@@ -36,43 +49,40 @@ function markAdLocation(lat, long) {
     }
     var nearbyAds = searchAdvertisements(oSearch);
     if(nearbyAds.length) {
-        var marker = new google.maps.Marker({
-            position: latLong
-        });
-        marker.setMap(map);
-        var contentString = '<div id="content">'+
-            '<div id="siteNotice">'+
-            '</div>'+
-            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-            '<div id="bodyContent">'+
-            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-            'sandstone rock formation in the southern part of the '+
-            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-            'south west of the nearest large town, Alice Springs; 450&#160;km '+
-            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-            'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-            'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-            'Aboriginal people of the area. It has many springs, waterholes, '+
-            'rock caves and ancient paintings. Uluru is listed as a World '+
-            'Heritage Site.</p>'+
-            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-            '(last visited June 22, 2009).</p>'+
-            '</div>'+
-            '</div>';
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
-        marker.addListener("click", function () {
-            infowindow.content = 'Custom content';
-            infowindow.open(map, marker);
-        });
-        marker.addListener("mouseover", function () {
-            infowindow.content = 'Custom content';
-            infowindow.open(map, marker);
-        });
-        marker.addListener("mouseout", function () {
-            infowindow.close();
+        nearbyAds.forEach(function (objAd) {
+            var latLong = new google.maps.LatLng(objAd.advertisementCoordinates.latitude, objAd.advertisementCoordinates.longitude),
+                icon = new google.maps.MarkerImage(
+                    "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|",
+                    new google.maps.Size(40, 40)
+                ),
+                marker = new google.maps.Marker({
+                    position: latLong,
+                    icon: icon
+                });
+            marker.setMap(map);
+            var contentString = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h1 id="firstHeading" class="firstHeading">'+ objAd.advertisementTitle +'</h1>'+
+                '<div id="bodyContent">'+
+                '<p>Price = '+ objAd.price +'</p>'+
+                '</div>'+
+                '</div>';
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+            marker.addListener("click", function () {
+                //infowindow.content = 'Custom content';
+                infowindow.open(map, marker);
+            });
+            searchedAds.push(marker);
+            /*marker.addListener("mouseover", function () {
+                infowindow.content = 'Custom content';
+                infowindow.open(map, marker);
+            });
+            marker.addListener("mouseout", function () {
+                infowindow.close();
+            });*/
         });
     }
 }
